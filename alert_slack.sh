2854691e3b8dbf99d05211f2309a5e2396086245
Parallel Works app-run user <gtawsdemo@gt-user-1.c.modular-magpie-167320.internal>
2022-07-08 15:10:53 +0000

    Fix typo

--- alert_slack.sh
+++ alert_slack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text": "__MSG__"}' https://hooks.slack.com/services/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"
+curl -X POST -H 'Content-type: application/json' --data '{"text": "__MSG__"}' https://hooks.slack.com/services/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
