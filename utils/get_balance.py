9a12b7bed1161e23bc2c4958c1193088ba21c118
avidalto <avidalto@hawk.iit.edu>
2024-04-09 09:35:02 +0000

    Update to new API headers

--- utils/get_balance.py
+++ utils/get_balance.py
@@ -3,13 +3,23 @@ import os
 import requests
 import argparse
 import json
+from base64 import b64encode
+
+def encode_string_to_base64(text):
+    # Convert the string to bytes
+    text_bytes = text.encode('utf-8')
+    # Encode the bytes to base64
+    encoded_bytes = b64encode(text_bytes)
+    # Convert the encoded bytes back to a string
+    encoded_string = encoded_bytes.decode('utf-8')
+    return encoded_string
 
 # This script needs to run in the user container to access the PW_API_KEY
 # Therefore, it is called by the main script using the reverse ssh tunnel
 # Prints the balance in json format
 
 PW_PLATFORM_HOST = os.environ.get('PW_PLATFORM_HOST')
-PW_API_KEY = os.environ.get('PW_API_KEY')
+HEADERS = {"Authorization": "Basic {}".format(encode_string_to_base64(os.environ['PW_API_KEY']))}
 # FIXME: Get from license server
 GT_PRODUCTS = ['gtsuite', 'gtautoliononed', 'gtpowerxrt']
 
@@ -51,9 +61,9 @@ if __name__ == '__main__':
 
     group_names = [f'{customer_name}-{gt_prod}' for gt_prod in GT_PRODUCTS]
 
-    GT_ORGANIZATION_URL = f'https://{PW_PLATFORM_HOST}/api/v2/organization/teams?organization={customer_org_id}&key={PW_API_KEY}'
+    GT_ORGANIZATION_URL = f'https://{PW_PLATFORM_HOST}/api/v2/organization/teams?organization={customer_org_id}'
 
-    res = requests.get(GT_ORGANIZATION_URL)
+    res = requests.get(GT_ORGANIZATION_URL, headers = HEADERS)
     existing_groups = [group['name'] for group in res.json()]
 
     check_group_existence(group_names, existing_groups)
