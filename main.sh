adb8d1b7866ba64783ab0bd1e802b66147dc21d2
avidalto <avidalto@hawk.iit.edu>
2024-08-27 10:48:54 -0500

    Remove custom sshcmd

--- main.sh
+++ main.sh
@@ -26,7 +26,7 @@ if ! [ -f "resources/host/inputs.sh" ]; then
 fi
 
 source resources/host/inputs.sh
-export sshcmd="ssh -A -o StrictHostKeyChecking=no ${resource_publicIp}"
+#export sshcmd="ssh -A -o StrictHostKeyChecking=no ${resource_publicIp}"
 
 cluster_rsync_exec
 
