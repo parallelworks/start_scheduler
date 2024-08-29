1f46c608b6985dd4aabb43265a0db42a81ac8b7e
avidalto <avidalto@hawk.iit.edu>
2024-08-29 09:52:38 -0500

    Exit workflow if connection to license server breaks

--- resources/host/scheduler.sh
+++ resources/host/scheduler.sh
@@ -25,6 +25,18 @@ mkdir -p ${sched_work_dir}/gtdistd ${sched_work_dir}/db ${sched_work_dir}/compou
 
 ulimit -u
 
+# Check if SSH access is available using the jumphost
+ssh -q -o BatchMode=yes -J usercontainer ${resource_ssh_usercontainer_options} ${gt_license_user}@${gt_license_ip} exit
+
+# Exit if SSH connection fails
+if [ $? -ne 0 ]; then
+    echo; echo
+    echod "ERROR: Controller has no SSH access to the license server." 
+    echod "       ssh -J usercontainer ${resource_ssh_usercontainer_options} ${gt_license_user}@${gt_license_ip}"
+    exit 1
+fi
+
+
 # Create license tunnels
 # FIXME: It assumes ~/.ssh/config is present and defines the usercontainer host!
 #        Won't work in onprem resources!
