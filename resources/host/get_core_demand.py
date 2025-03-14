dae8828fbc8b04352bee34ee10e51ac0c2db2b6d
avidalto <avidalto@hawk.iit.edu>
2025-03-14 06:22:23 -0500

    Print warning to stderr

--- resources/host/get_core_demand.py
+++ resources/host/get_core_demand.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 from copy import deepcopy
 import xml.etree.ElementTree as ET
 import argparse
@@ -202,10 +203,10 @@ def remove_jobs_with_no_balance(active_jobs, balance):
             # In some cases the job license is unkown at this point
             continue
         elif job_license not in balance:
-            print('Warning: Removing job with license {job_license} not present in balance', job, balance, flush = True)
+            print('Warning: Removing job with license {job_license} not present in balance', job, balance, file=sys.stderr, flush = True)
             del active_jobs_with_balance[job]
         elif balance[job_license] <= 0:
-            print('Warning: Removing job with license {job_license} ane negative balance', job, balance, flush = True)
+            print('Warning: Removing job with license {job_license} ane negative balance', job, balance, file=sys.stderr, flush = True)
             del active_jobs_with_balance[job]
 
     return active_jobs_with_balance
