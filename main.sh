6d0b451e7a8525da3bbb4b0c23bf05e513fa298d
avidalto <avidalto@hawk.iit.edu>
2025-05-09 04:32:34 -0500

    debugging

--- main.sh
+++ main.sh
@@ -49,6 +49,8 @@ if [ ${return_code} -ne 0 ]; then
     exit 1
 fi
 
+echo sleeping
+sleep 1000
 # Launch scheduler
 echo; echo
 echo "ssh -A -o StrictHostKeyChecking=no ${resource_publicIp} ${resource_jobdir}/${resource_label}/launch_scheduler.sh"
